32b92ca72fc62f06d603d1a8d5a3f1c359e687c9
AEIENG\hhofer <hhofer@aeieng.com>
2017-06-24 15:29:42 -0400

    no message

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function () {
+   return [
+     { title: 'Javascript: The Good Parts' },
+     { title: 'Invisible Man' },
+     { title: 'To the Lighthouse' },
+     { title: 'A Long Loneliness' }
+   ]
+}
